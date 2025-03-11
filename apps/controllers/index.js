31497487c088b004cb6a5884d004f3cbb03f9ffd
NgoTriNgocSon <ngoson17102003@gmail.com>
2025-03-11 10:12:14 +0700

    update profile ca nhan cua Son

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -2,7 +2,14 @@ var express = require("express");
 var router = express.Router();
 router.use("/home", require (__dirname + "/homecontroller"));
 router.use("/admin", require(__dirname + "/admin/admincontroller"));
+router.use("/admin", require(__dirname + "/admin/admincontroller"));
 router.get("/", function(req,res){
     res.render("index.ejs");
+});
+router.get("/Son", function (req, res) {
+    res.render("Son.ejs", {
+        title: "Trang của Sơn",
+        message: "Chào mừng đến với trang của Sơn!"
+    });
 });
 module.exports = router;
