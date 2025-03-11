462749bbb0d01122f931eccfa65913a9a9f2d56e
Tien264 <tienvinhtrinh@gmail.com>
2025-03-11 10:00:44 +0700

    update chuc nang cua Tien

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -4,5 +4,9 @@ router.use("/home", require (__dirname + "/homecontroller"));
 router.use("/admin", require(__dirname + "/admin/admincontroller"));
 router.get("/", function(req,res){
     res.render("index.ejs");
+
+});
+router.get("/tien", function(req,res){
+    res.render("tien.ejs");
 });
 module.exports = router;
